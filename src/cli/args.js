82b35d897bd899298f35b03d58dbe80336d1901d
dustytrail33 <dustytrail33@mail.ru>
2025-04-27 13:44:24 +0300

    feat: added solution for cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+
+  const argsList = args
+    .reduce((list, arg, index) => {
+      if (arg.startsWith("--")) {
+        const prop = arg.replace("--", "");
+        const value = args[index + 1];
+
+        list.push(`${prop} is ${value}`);
+      }
+      return list;
+    }, [])
+    .join(", ");
+  console.log(argsList);
 };
 
 parseArgs();
